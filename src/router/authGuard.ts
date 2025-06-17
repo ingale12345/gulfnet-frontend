04cccc6a53073b15d3d210a578e1d37bbbc65a8e
ingale12345 <ingaleprajawal@.com>
2025-06-17 15:11:18 +0530

    added default value for isAuthenticated and role .. bcz we dont have auth setup

--- src/router/authGuard.ts
+++ src/router/authGuard.ts
@@ -1,9 +1,10 @@
 // router/authGuard.ts
 import { redirect } from "react-router-dom";
-import { useAuthStore } from "@/store/authStore";
+// import { useAuthStore } from "@/store/authStore";
 
 export const authGuard = (allowedRoles: string[]) => () => {
-  const { isAuthenticated, role } = useAuthStore.getState();
+  // const { isAuthenticated, role } = useAuthStore.getState();
+  const { isAuthenticated, role } = { isAuthenticated: true, role: "hq_admin" }; // added temporary default values
 
   if (!isAuthenticated) return redirect("/login");
   if (!allowedRoles.includes(role!)) return redirect("/unauthorized");
